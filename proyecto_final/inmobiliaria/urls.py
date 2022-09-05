18042e33fa5f4851baf57e22148d332e559a2c22
martingutoli <martingutoli@gmail.com>
2022-09-05 04:49:16 -0300

    creacion de boton busqueda, y login fallido

--- proyecto_final/inmobiliaria/urls.py
+++ proyecto_final/inmobiliaria/urls.py
@@ -6,5 +6,9 @@ urlpatterns =[
     path("alquileres/", alquileres, name = "alquileres"),
     path("ventas/", ventas, name = "ventas"),
     path("about/", about, name = "about"),
+    path("busqueda", form_busqueda, name = "busqueda"),
+    path("resultados/", buscar, name = "resultado_busqueda"),
+    path("login/", iniciar_sesion, name = "login"),
+    path("registro/", registrar, name = "registro")
 
 ]
--- proyecto_final/inmobiliaria/views.py
+++ proyecto_final/inmobiliaria/views.py
@@ -1,8 +1,9 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from inmobiliaria.models import Alquileres, Ventas
-
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 
@@ -26,3 +27,66 @@ def ventas(request):
 
 def about(request):
     return render(request, "inmobiliaria/about.html")
+
+
+def form_busqueda(request):
+    return render(request, "inmobiliaria/form_busqueda.html")
+
+def buscar (request):
+    inmueble_tipo = request.GET.get('descripcion', None)
+
+    if not inmueble_tipo:
+        return HttpResponse ("No indicaste nada")
+    inmuebles_lista = Alquileres.objects.filter(descripcion__icontains=inmueble_tipo)
+    return render(request, "inmobiliaria/resultado_busqueda.html", {"inmuebles": inmuebles_lista})
+
+def iniciar_sesion(request):
+    if request.method == "GET":
+        formulario = AuthenticationForm()
+
+        context ={
+
+            "form" : formulario
+        }
+
+        return render(request, "inmobiliaria/login.html", context)
+    else:
+        formulario = AuthenticationForm(request, data = request.POST)
+
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+            usuario = authenticate(username=data.get("username"), password=data.get("password"))
+
+            if usuario is not None:
+                login  (request, usuario)
+                return redirect("inicio")
+            else:
+                context = {
+                    "error" : "Credenciales no validas",
+                    "form" : formulario
+                }
+                
+                return render(request, "inmobiliaria/login.html", context)
+        else:
+
+            context = {
+                    "error" : "Formulario no valido",
+                    "form" : formulario
+            }
+                
+            return render(request, "inmobiliaria/login.html", context)
+
+    return HttpResponse()
+
+def registrar(request):
+
+    if request.method == "GET":
+        formulario = UserCreationForm()
+        return render(request, 'inmobiliaria/registro.html', {"form": formulario})
+    else:
+        formulario = UserCreationForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect("inicio")
+        else:
+            return render(request, "inmobiliaria/registro.html", {"form": formulario, "error": "Formulario no valido"})
